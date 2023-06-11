e6b50ce4f9af9f1a8c81668c9cb4f0740f50534d
REZAUL KARIM <129675999+rezaul003@users.noreply.github.com>
2023-06-11 19:41:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example'
+})
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
